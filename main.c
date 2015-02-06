2fbebf83de3911575f17b9725ae8967f7658bafd
Erik Svedäng <erik.svedang@gmail.com>
2015-02-06 20:02:17 +0100

    parser changes...

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 
 typedef struct {
   int pos;
+  Obj *first_form;
+  Obj *last_form;
 } Parser;
 
 Parser *parser_make() {
@@ -17,47 +19,67 @@ Parser *parser_make() {
   return p;
 }
 
+Obj *parse_sexp(GC *gc, Parser *p, const char *source);
+
+Obj *parse_form(GC *gc, Parser *p, const char *source) {
+  if (source[p->pos] == '(') {
+    p->pos++;
+    return parse_sexp(gc, p, source);
+  }
+  else if(isalpha(source[p->pos])) {
+    while(isalpha(source[p->pos])) {
+      p->pos++;
+    }
+    return gc_make_symbol(gc, "?");
+  }
+  p->pos++;
+
+  return NULL;
+}
+
+Obj *parse_sexp(GC *gc, Parser *p, const char *source) {
+  Obj *sexp = gc_make_cons(gc, NULL, NULL);
+  Obj *lastSexp = sexp;
+ again:;
+  Obj *inner = parse_form(gc, p, source); 
+  lastSexp->car = inner;
+  Obj *next = gc_make_cons(gc, NULL, NULL);
+  lastSexp->cdr = next;
+  lastSexp = next;
+  if(source[p->pos] == ' ') {
+    p->pos++;
+    goto again;
+  }
+  else if(source[p->pos] != ')') {
+    printf("Failed to parse %c at %d\n", source[p->pos], p->pos);
+    exit(1);
+  }
+  return sexp;
+}
+
 Obj *parse(GC *gc, const char *source) {
   Parser* p = parser_make();
   int source_len = strlen(source);
   int parens = 0;
 
-  Obj *forms = gc_make_cons(gc, NULL, NULL);
-  Obj *lastForm = forms;
+  p->first_form = gc_make_cons(gc, NULL, NULL);
+  p->last_form = p->first_form;
   
   while(p->pos < source_len) {
-    if (source[p->pos] == '(') {
-      p->pos++;
-      Obj *sexp = gc_make_cons(gc, NULL, NULL);
-      Obj *lastSexp = sexp;
-    again:
-      while(isalpha(source[p->pos])) {
-	p->pos++;
-      }
-      Obj *inner = gc_make_symbol(gc, "?");
-      lastSexp->car = inner;
-      Obj *next = gc_make_cons(gc, NULL, NULL);
-      lastSexp->cdr = next;
-      lastSexp = next;
-      if(source[p->pos] == ' ') {
-	p->pos++;
-	goto again;
-      }
-      else if(source[p->pos] != ')') {
-	printf("Failed to parse %c at %d\n", source[p->pos], p->pos);
-	exit(1);
-      }
-      lastForm->car = sexp;
-      Obj* newCell = gc_make_cons(gc, NULL, NULL);
-      lastForm->cdr = newCell;
-      lastForm = newCell;
+    Obj *form = parse_form(gc, p, source);
+    if(form) {
+      printf("Got form: ");
+      print_obj(form);
+      printf("\n");
+      p->last_form->car = form;
+      p->last_form->cdr = gc_make_cons(gc, NULL, NULL);
+      p->last_form = p->last_form->cdr;
     }
-    p->pos++;
   }
 
   //printf("%d parens.\n", parens);
  
-  return forms;
+  return p->first_form;
 }
 
   
@@ -68,7 +90,7 @@ int main()
   //test_printing();
   
   GC gc;
-  Obj *forms = parse(&gc, "(aha boo cic) (d e) (f) g h i");
+  Obj *forms = parse(&gc, "a b c (d e) ((f g g) (h i) (j k))"); //(aha boo cic) (d e) (f) g h i");
   print_obj(forms);
 }
 
