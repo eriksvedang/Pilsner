a5b72d32104365ee9a64d3f1f0ffc15436385015
Erik Svedäng <erik.svedang@gmail.com>
2015-02-13 15:02:04 +0100

    repl stuff

--- Repl.h
+++ Repl.h
@@ -1,6 +1,7 @@
 #ifndef REPL_H
 #define REPL_H
 
+#include <assert.h>
 #include "GC.h"
 #include "Obj.h"
 #include "Parser.h"
@@ -20,10 +21,15 @@ void repl() {
     fgets(str, BUFFER_SIZE, stdin);
     runtime_eval(r, str);
   }
+
+  // Pop global environment
+  assert(r->gc->stackSize == 1);
+  gc_stack_pop(r->gc);
   
-  //runtime_inspect_env(r);
   gc_collect(r->gc);
   runtime_delete(r);
+
+  //fgets(str, BUFFER_SIZE, stdin);
 }
 
 #endif
