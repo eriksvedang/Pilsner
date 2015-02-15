c2c250430451b911271e4dde1b82200e71aa0c62
Erik Svedäng <erik.svedang@gmail.com>
2015-02-15 15:34:00 +0100

    cleanup

--- Repl.h
+++ Repl.h
@@ -7,44 +7,41 @@
 #include "Parser.h"
 #include "Runtime.h"
 
+void load(Runtime *r, const char *lib_path, const char *filename) {
+  char full_path[2048];
+  sprintf(full_path, "%s%s", lib_path, filename);
+  runtime_load_file(r, (const char *)full_path, true);
+}
+
 void repl() {
 
   char *lib_path = getenv("PILSNER_LIB");
-  if(lib_path) {
-    //printf("PILSNER_LIB=%s\n", lib_path);
-  } else {
+  if(!lib_path) {
     error("The environment variable PILNSER_LIB is not set.");
   } 
   
   Runtime *r = runtime_new();
 
-  char full_path[2048];
-  
-  sprintf(full_path, "%s%s", lib_path, "core.lisp");
-  runtime_load_file(r, (const char *)full_path, true);
+  load(r, lib_path, "core.lisp");
+  load(r, lib_path, "misc.lisp");
 
-  sprintf(full_path, "%s%s", lib_path, "misc.lisp");
-  runtime_load_file(r, (const char *)full_path, true);
-
-  const int BUFFER_SIZE = 2048;
-  char str[BUFFER_SIZE];
+  const int MAX_INPUT_BUFFER_SIZE = 2048;
+  char str[MAX_INPUT_BUFFER_SIZE];
   
   while(r->mode != RUNTIME_MODE_FINISHED) {
-    //runtime_print_frames(r);
-    //gc_stack_print(r->gc);
     printf("> ");
-    fgets(str, BUFFER_SIZE, stdin);
+    fgets(str, MAX_INPUT_BUFFER_SIZE, stdin);
     runtime_eval(r, str);
   }
 
-  // Pop global environment
+  // The global environment should be the only thing on the stack
   assert(r->gc->stackSize == 1);
   gc_stack_pop(r->gc);
   
   gc_collect(r->gc);
   runtime_delete(r);
 
-  fgets(str, BUFFER_SIZE, stdin);
+  fgets(str, MAX_INPUT_BUFFER_SIZE, stdin);
 }
 
 #endif
